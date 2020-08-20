f9d02ba5543accac64ec5723e2ceb53fd13a89dc
Christian Piazzi <christian.piazzi@gmx.de>
2020-08-20 19:14:46 +0200

    add index page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
+from flask import render_template
 from app import app
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    response = requests.get("http://localhost:9000/",auth=HTTPBasicAuth('admin', 'admin'))
+
+    decoded = json.loads(response.content.decode())
+
+    return render_template('index.html', title='Home', data=decoded)
