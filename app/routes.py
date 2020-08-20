e32fc76aa48001072ace962592957912c0961a2f
Christian Piazzi <christian.piazzi@gmx.de>
2020-08-20 19:29:36 +0200

    add yaml config as workaround for real config parser

--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,15 @@ from app import app
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+import yaml
+
+with open("app/config.yml", "r") as ymlfile:
+    cfg = yaml.load(ymlfile)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    response = requests.get("http://localhost:9000/",auth=HTTPBasicAuth('admin', 'admin'))
+    response = requests.get('http://' + cfg['docker-agent']['host'] + ':' +cfg['docker-agent']['port'].__str__(),auth=HTTPBasicAuth('admin', 'admin'))
 
     decoded = json.loads(response.content.decode())
 
